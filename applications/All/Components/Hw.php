82113a65a7508eefa772038bf9cd3b96f00e9a8b
Gurdeep Bagga (Guru) <guru@bazaari.com.au>
2020-09-13 17:56:11 +0530

    Initial Commit

---
+++ applications/All/Components/Hw.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Applications\All\Components;
+
+use System\Base\Controller;
+
+class Hw extends Controller
+{
+	public function view()
+	{
+		return $this->generateView();
+	}
+}
